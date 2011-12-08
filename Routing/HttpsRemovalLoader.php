e40d6b95019dfaaa7ee66e6df53585dd0ccbf6c9
Oleg Zinchenko <olegz@default-value.com>
2011-12-08 21:50:39 +0200

    adapted for new symfony version

--- Routing/HttpsRemovalLoader.php
+++ Routing/HttpsRemovalLoader.php
@@ -3,7 +3,7 @@
 namespace Jmikola\InsecureRoutesBundle\Routing;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
 
 class HttpsRemovalLoader implements LoaderInterface
 {
@@ -59,7 +59,7 @@ class HttpsRemovalLoader implements LoaderInterface
     /**
      * @see Symfony\Component\Config\Loader\LoaderInterface::setResolver()
      */
-    public function setResolver(LoaderResolver $resolver)
+    public function setResolver(LoaderResolverInterface $resolver)
     {
         $this->loader->setResolver($resolver);
     }
